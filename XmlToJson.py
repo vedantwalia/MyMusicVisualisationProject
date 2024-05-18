88718df79a0b48f2bae93229c40b4ac55d0e587c
Vedant Walia <vedantwalia@outlook.com>
2024-05-18 10:33:08 +0530

    Cleaning repo and merging scripts

--- XmlToJson.py
+++ XmlToJson.py
@@ -1,7 +1,20 @@
+import xml.etree.ElementTree as ET
 import plistlib
 import json
 
-# Load plist file
+
+# Parse the XML file
+tree = ET.parse('Library.xml')
+root = tree.getroot()
+
+# Replace <date> tags with <string>
+for elem in root.iter():
+    if elem.tag == 'date':
+        elem.tag = 'string'
+
+# Write modified XML content to a new file
+tree.write('Library_cleaned.xml')
+
 with open('Library_cleaned.xml', 'rb') as f:
     plist_data = plistlib.load(f)
 
@@ -11,3 +24,33 @@ json_data = json.dumps(plist_data)
 # Write to JSON file
 with open('Library_cleaned.json', 'w') as f:
     f.write(json_data)
+
+
+def add_missing_keys_in_tracks(tracks_data, expected_keys):
+    for track_id, track_info in tracks_data.items():
+        for key in expected_keys:
+            if key not in track_info:
+                track_info[key] = 0
+
+def check_and_update_json(json_file_path, expected_keys):
+    # Load JSON file
+    with open(json_file_path, 'r') as file:
+        data = json.load(file)
+
+    # Check if "Tracks" key exists
+    if "Tracks" in data:
+        tracks_data = data["Tracks"]
+        # Add missing keys in each track
+        add_missing_keys_in_tracks(tracks_data, expected_keys)
+    else:
+        print("No 'Tracks' key found in the JSON file.")
+
+    # Write the updated JSON back to the file
+    with open(json_file_path, 'w') as file:
+        json.dump(data, file, indent=4)
+
+
+# Example usage:
+json_file_path = "Library_cleaned.json"  # Replace with the path to your JSON file
+expected_keys = ["Play Count"]  # Add more keys as needed
+check_and_update_json(json_file_path, expected_keys)
